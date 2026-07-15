a7f0cfcbd8ca806cc87fe870e91c7815f7aa5843
Sam <fupenghui6@gmail.com>
2026-07-15 21:03:20 +0800

    Update TimesNet.sh

--- TimesNet.sh
+++ TimesNet.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
-cd /home/zhao/fu/Time-Series-Library-main/
+
 model_name=TimesNet
 
 python -u run.py \
@@ -27,78 +27,78 @@ python -u run.py \
   --des 'Exp' \
   --itr 1
 
-#
-#python -u run.py \
-#  --task_name long_term_forecast \
-#  --is_training 1 \
-#  --root_path ./dataset/cite30MW/ \
-#  --data_path 1-6.csv \
-#  --model_id solar1_96_192 \
-#  --model $model_name \
-#  --data custom \
-#  --features M \
-#  --seq_len 96 \
-#  --label_len 48 \
-#  --pred_len 192 \
-#  --e_layers 2 \
-#  --d_layers 1 \
-#  --factor 3 \
-#  --enc_in 6 \
-#  --dec_in 6 \
-#  --c_out 6 \
-#  --d_model 32 \
-#  --d_ff 32 \
-#  --top_k 5 \
-#  --des 'Exp' \
-#  --itr 1 \
-#  --train_epochs 1
-#
-#
-#python -u run.py \
-#  --task_name long_term_forecast \
-#  --is_training 1 \
-#  --root_path ./dataset/cite30MW/ \
-#  --data_path 1-6.csv \
-#  --model_id solar1_96_336 \
-#  --model $model_name \
-#  --data custom \
-#  --features M \
-#  --seq_len 96 \
-#  --label_len 48 \
-#  --pred_len 336 \
-#  --e_layers 2 \
-#  --d_layers 1 \
-#  --factor 3 \
-#  --enc_in 6 \
-#  --dec_in 6 \
-#  --c_out 6 \
-#  --d_model 32 \
-#  --d_ff 32 \
-#  --top_k 5 \
-#  --des 'Exp' \
-#  --itr 1
-#
-#python -u run.py \
-#  --task_name long_term_forecast \
-#  --is_training 1 \
-#  --root_path ./dataset/cite30MW/ \
-#  --data_path 1-6.csv \
-#  --model_id solar1_96_720 \
-#  --model $model_name \
-#  --data custom \
-#  --features M \
-#  --seq_len 96 \
-#  --label_len 48 \
-#  --pred_len 720 \
-#  --e_layers 2 \
-#  --d_layers 1 \
-#  --factor 3 \
-#  --enc_in 6 \
-#  --dec_in 6 \
-#  --c_out 6 \
-#  --d_model 32 \
-#  --d_ff 32 \
-#  --top_k 5 \
-#  --des 'Exp' \
-#  --itr 1 \
-#  --train_epochs 1
+
+python -u run.py \
+ --task_name long_term_forecast \
+ --is_training 1 \
+ --root_path ./dataset/cite30MW/ \
+ --data_path 1-6.csv \
+ --model_id solar1_96_192 \
+ --model $model_name \
+ --data custom \
+ --features M \
+ --seq_len 96 \
+ --label_len 48 \
+ --pred_len 192 \
+ --e_layers 2 \
+ --d_layers 1 \
+ --factor 3 \
+ --enc_in 6 \
+ --dec_in 6 \
+ --c_out 6 \
+ --d_model 32 \
+ --d_ff 32 \
+ --top_k 5 \
+ --des 'Exp' \
+ --itr 1 \
+ --train_epochs 1
+
+
+python -u run.py \
+ --task_name long_term_forecast \
+ --is_training 1 \
+ --root_path ./dataset/cite30MW/ \
+ --data_path 1-6.csv \
+ --model_id solar1_96_336 \
+ --model $model_name \
+ --data custom \
+ --features M \
+ --seq_len 96 \
+ --label_len 48 \
+ --pred_len 336 \
+ --e_layers 2 \
+ --d_layers 1 \
+ --factor 3 \
+ --enc_in 6 \
+ --dec_in 6 \
+ --c_out 6 \
+ --d_model 32 \
+ --d_ff 32 \
+ --top_k 5 \
+ --des 'Exp' \
+ --itr 1
+
+python -u run.py \
+ --task_name long_term_forecast \
+ --is_training 1 \
+ --root_path ./dataset/cite30MW/ \
+ --data_path 1-6.csv \
+ --model_id solar1_96_720 \
+ --model $model_name \
+ --data custom \
+ --features M \
+ --seq_len 96 \
+ --label_len 48 \
+ --pred_len 720 \
+ --e_layers 2 \
+ --d_layers 1 \
+ --factor 3 \
+ --enc_in 6 \
+ --dec_in 6 \
+ --c_out 6 \
+ --d_model 32 \
+ --d_ff 32 \
+ --top_k 5 \
+ --des 'Exp' \
+ --itr 1 \
+ --train_epochs 1
